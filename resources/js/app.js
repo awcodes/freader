f354312c32bca0876cec4ff614fc9d732192ec44
awcodes <adam@adamwgd.com>
2022-02-28 12:28:20 -0500

    wip: install tailwindcss and alpinejs and run initial build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import Alpine from "alpinejs";
+
+/**
+ * Axios
+ */
+window.axios = require("axios");
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+/**
+ * Alpine
+ */
+window.Alpine = Alpine;
+Alpine.start();
